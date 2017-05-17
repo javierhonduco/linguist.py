33efe887fef084e844a359dd9831a5bfdfc12a24
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-05-17 16:45:09 +0200

    add simple benchmarking

---
+++ benchmark.sh
@@ -0,0 +1,18 @@
+echo "==> Benchmarking on demand process spawning..."
+time python -c "
+from main import popen_helper, write_to_process, ruby_binary
+
+for i in range(100):
+  popen = popen_helper(ruby_binary())
+  print(write_to_process(popen, b'../linguist\n'))
+" > /dev/null
+
+
+echo "==> Benchmarking long-running subprocess with IPC..."
+time python -c "
+from main import popen_helper, write_to_process, ruby_binary
+
+popen = popen_helper(ruby_binary())
+for i in range(100):
+  print(write_to_process(popen, b'../linguist\n'))
+" > /dev/null
--- main.py
+++ main.py
@@ -8,17 +8,19 @@ def write_to_process(process, data):
     process.stdin.write(b'\n')
     process.stdin.flush()
 
-    read_line = p.stdout.readline()
+    read_line = process.stdout.readline()
     read_line = read_line.strip().decode()
 
     return json.loads(read_line)
 
+def popen_helper(ruby_binary):
+    return Popen([ruby_binary, 'linguist_server.rb'], stdin=PIPE, stdout=PIPE)
+
 default_ruby_binary = '/Users/javierhonduco/.rbenv/shims/ruby'
-ruby_binary = os.environ.get('RUBY_BINARY', default_ruby_binary)
 
-p = Popen([ruby_binary, 'linguist_server.rb'], stdin=PIPE, stdout=PIPE)
+def ruby_binary():
+    return os.environ.get('RUBY_BINARY', default_ruby_binary)
 
-
-print(write_to_process(p, b'../\n'))
-print(write_to_process(p, b'../linguist\n'))
-print(write_to_process(p, b'../\n'))
+if __name__ == '__main__':
+    popen = popen_helper(ruby_binary())
+    print(write_to_process(popen, b'../linguist\n'))
